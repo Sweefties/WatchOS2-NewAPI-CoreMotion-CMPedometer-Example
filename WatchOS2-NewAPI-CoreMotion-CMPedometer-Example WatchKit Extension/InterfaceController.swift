0501ec9953d8691e72533c2d3e2ac452ef4af229
Wlad Dicario <wlad@2v-prod.com>
2015-09-03 02:15:50 +0200

    Initial Commit

---
+++ WatchOS2-NewAPI-CoreMotion-CMPedometer-Example WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchOS2-NewAPI-CoreMotion-CMPedometer-Example WatchKit Extension
+//
+//  Created by Wlad Dicario on 03/09/2015.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
