ffa17ad817f8c0fda91afb1fe1bd05da2cb108fc
Wlad Dicario <wlad@2v-prod.com>
2015-09-07 00:09:48 +0200

    Initial Commit.

--- WatchOS2-NewAPI-CoreMotion-CMPedometer-Example WatchKit Extension/InterfaceController.swift
+++ WatchOS2-NewAPI-CoreMotion-CMPedometer-Example WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,100 @@
 
 import WatchKit
 import Foundation
-
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
 
+    // MARK: - Interface
+    @IBOutlet var stepsLbl: WKInterfaceLabel!
+    @IBOutlet var stepsCount: WKInterfaceLabel!
+    @IBOutlet var floorsAscLbl: WKInterfaceLabel!
+    @IBOutlet var floorsAscCount: WKInterfaceLabel!
+    @IBOutlet var floorsDesLbl: WKInterfaceLabel!
+    @IBOutlet var floorsDesCount: WKInterfaceLabel!
+    @IBOutlet var distanceLbl: WKInterfaceLabel!
+    @IBOutlet var distanceCount: WKInterfaceLabel!
+    @IBOutlet var cadenceLbl: WKInterfaceLabel!
+    @IBOutlet var cadenceCount: WKInterfaceLabel!
+    @IBOutlet var paceLbl: WKInterfaceLabel!
+    @IBOutlet var paceCount: WKInterfaceLabel!
+    
+    // MARK: - Properties
+    private let corePedometer = CMPedometer()
+    
+    
+    // MARK: - Context Initializer
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
         // Configure interface objects here.
+        
+        // set text labels
+        self.stepsCount.setText("-")
+        self.floorsAscCount.setText("-")
+        self.floorsDesCount.setText("-")
+        self.distanceCount.setText("- m.")
+        self.cadenceCount.setText("- m/s")
+        self.paceCount.setText("- sec/m")
     }
+    
 
+    // MARK: - Calls
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        // to start pedometer updates..
+        startPedometerUpdates()
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        
+        // to stop pedometer updates.. eg:now offscreen
+        self.corePedometer.stopPedometerUpdates()
+    }
+    
+    
+    // MARK: - Get Pedometer Updates
+    func startPedometerUpdates(){
+        // define start date
+        let startDate = NSDate()
+        
+        // get CMPedometerData updates from startDate
+        if CMPedometer.isPaceAvailable() {
+            
+            self.corePedometer.startPedometerUpdatesFromDate(startDate, withHandler: { (data:CMPedometerData?, error:NSError?) -> Void in
+                
+                if (error == nil) {
+                    
+                    // rounded distance value (in meter eg: 12.54)
+                    let distance = data!.distance!.doubleValue.m
+                    let np = 2.0
+                    let multi = pow(10.0, np)
+                    let rounded = round(distance * multi) / multi
+                    print("Distance standard rounding : \(rounded)m")
+                    
+                    // set text labels
+                    self.stepsCount.setText("\(data!.numberOfSteps ?? 0)")
+                    self.floorsAscCount.setText("\(data!.floorsAscended! ?? 0)")
+                    self.floorsDesCount.setText("\(data!.floorsDescended! ?? 0)")
+                    self.distanceCount.setText("\(rounded ?? 0)m.")
+                    self.cadenceCount.setText("\(data!.currentCadence! ?? 0)m/s")
+                    self.paceCount.setText("\(data!.currentPace! ?? 0)sec/m")
+                }
+            })
+        }
     }
 
+}
+
+
+//MARK: - Double Units Extension
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
 }
--- WatchOS2-NewAPI-CoreMotion-CMPedometer-Example/ViewController.swift
+++ WatchOS2-NewAPI-CoreMotion-CMPedometer-Example/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    // MARK: - Interface
+    @IBOutlet weak var pedometerLabel: UILabel!
+    
+    // MARK: - Constants
+    private let corePedometer = CMPedometer()
+    
+    // MARK: - Properties
+    private var steps = String()
+    private var floorsAsc = String()
+    private var floorsDes = String()
+    private var distance = String()
+    private var cadence = String()
+    private var pace = String()
+    
+    // MARK: - Calls
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // start Pedometer updates
+        setPedometer()
     }
 
+    // MARK: - Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Get Pedometer Updates
+    func setPedometer(){
+        // define start date
+        let startDate = NSDate()
+        
+        // get the current pedometer updates
+        if CMPedometer.isStepCountingAvailable() {
+            self.corePedometer.startPedometerUpdatesFromDate(startDate, withHandler: { (data:CMPedometerData?, error:NSError?) -> Void in
+                
+                // GCD perform
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    if (error == nil) {
+                        
+                        // rounded distance value (kms)
+                        let distance = data!.distance!.doubleValue.mtokm
+                        let np = 2.0
+                        let multiplier = pow(10.0, np)
+                        let rounded = round(distance * multiplier) / multiplier
+                        print("Distance standard rounding : \(rounded) km.")
+                        
+                        
+                        // set values from CMPedometer Data
+                        self.steps = "Steps : \(data!.numberOfSteps)\n"
+                        self.floorsAsc = "Floors Ascended : \(data!.floorsAscended!)\n"
+                        self.floorsDes = "Floors Descended : \(data!.floorsDescended!)\n"
+                        self.distance = "Distance : \(rounded)kms\n"
+                        
+                        // states for availability
+                        if #available(iOS 9.0, *) {
+                            self.cadence = "Cadence : \(data!.currentCadence!)m/s\n"
+                            self.pace = "Pace : \(data!.currentPace!)sec/m\n"
+                        } else {
+                            // Fallback on earlier versions
+                            self.cadence = "Cadence : availability iOS 9.0+\n"
+                            self.pace = "Pace : availability iOS 9.0+\n"
+                        }
+                        
+                        // set text label
+                        self.pedometerLabel.text = self.steps + self.floorsAsc + self.floorsDes + self.distance + self.cadence + self.pace
+                    }
+                })
+            })
+        }
+    }
 
 }
 
+//MARK: - Double Units Extension
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var mtokm: Double { return self / 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
